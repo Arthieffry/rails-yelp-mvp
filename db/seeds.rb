56e61d8065f10e70e0d7f7d191627d3d6d0135ba
Arthur Thieffry <arthieffry@gmail.com>
2017-11-22 13:32:07 +0100

    1er commit fin de resto debut view review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do
+  a = Restaurant.create!(
+    name: Faker::Pokemon.name,
+    address: Faker::Address.street_address + ", " + Faker::Pokemon.location,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample,
+    phone_number: Faker::PhoneNumber.phone_number,
+    )
+  p a
+end
