5940337b6f35cba1ae6506b3b3eb51b97f35caa5
Arthur Thieffry <arthieffry@gmail.com>
2017-11-22 17:50:50 +0100

    Avancez sur front + correction bug form

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :restaurant
 
   validates :content, presence: true
-  validates_numericality_of :rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, default: 0
+  validates_numericality_of :rating, :greater_than_or_equal_to => 0, :less_than_or_equal_to => 5, default: 0, presence: true
 end
